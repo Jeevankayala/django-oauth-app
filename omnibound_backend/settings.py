94fe65d69a96abf71e714f599842097348a5e938
Jeevan-Omnibound <jeevan.kayala@omnibound.ai>
2025-04-24 18:38:27 +0530

    Configured project for Render deployment

--- omnibound_backend/settings.py
+++ omnibound_backend/settings.py
@@ -84,12 +84,12 @@ WSGI_APPLICATION = 'omnibound_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 import os
 from pathlib import Path
@@ -98,17 +98,17 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'mydb'),
-        'USER': os.getenv('DB_USER', 'myuser'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'mypassword'),
-        'HOST': os.getenv('DB_HOST'),
-        # 'HOST': 'db', # Docker service name
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME', 'mydb'),
+#         'USER': os.getenv('DB_USER', 'myuser'),
+#         'PASSWORD': os.getenv('DB_PASSWORD', 'mypassword'),
+#         'HOST': os.getenv('DB_HOST'),
+#         # 'HOST': 'db', # Docker service name
+#         'PORT': os.getenv('DB_PORT', '5432'),
+#     }
+# }
 
 
 # Password validation
