6fa62b886dcdcb8947894c161d2bf4e757667373
Jeevan-Omnibound <jeevan.kayala@omnibound.ai>
2025-04-24 23:16:52 +0530

    Optimized Gunicorn config, logging, and memory usage for Render

--- authentication/models.py
+++ authentication/models.py
@@ -20,6 +20,21 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+    
+    def create_superuser(self, email, clerk_id, password, **extra_fields):
+        """
+        Override create_superuser to allow username=None.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class GoogleToken(models.Model):
